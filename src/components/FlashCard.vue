13a154375aafa8ccb9c4e1f13293836335ec2ed0
yelizsevinc <yelizondessevinc@gmail.com>
2023-04-22 11:05:25 +0100

    added explanation field to FlashCard's

--- src/components/FlashCard.vue
+++ src/components/FlashCard.vue
@@ -7,34 +7,39 @@
     height="300"
     width="20vw"
   >
-    <v-card-title v-if="!isQuestionDisplayed">
-      <v-icon large left>mdi-comment-question</v-icon>
-      <span class="text-h6 font-weight-bold pl-2">{{data.question}}</span>
-    </v-card-title>
+    <div v-if="isQuestionDisplayed">
+      <v-card-text class="text-h5 font-weight-bold pt-12 mt-12">
+        <v-icon large left>mdi-comment-question</v-icon><span class="pl-2">{{data.question}}</span>
+      </v-card-text>
 
-    <v-card-text v-else class="text-h5 font-weight-bold pt-12 mt-12">
-      <v-icon large left>mdi-comment-question</v-icon><span class="pl-2">{{data.question}}</span>
-    </v-card-text>
+      <v-card-actions>
+        <v-row>
+          <v-chip
+            v-for="hint in data.hints"
+            :key="hint"
+            color="secondary"
+            class="ma-2 ml-4"
+          >
+            {{ hint }}
+          </v-chip>
+        </v-row>
+      </v-card-actions>
+    </div>
 
-    <v-card-text
-      v-if="!isQuestionDisplayed"
-      class="text-h6 font-weight-bold pt-12 mt-12"
-      style="white-space: pre-line"
-      >{{ data.answer }}</v-card-text
-    >
+    <div v-else>
+      <v-card-title>
+        <v-icon large left>mdi-comment-question</v-icon>
+        <span class="text-h6 font-weight-bold pl-2">{{data.question}}</span>
+      </v-card-title>
 
-    <v-card-actions v-if="isQuestionDisplayed">
-      <v-row>
-        <v-chip
-          v-for="hint in data.hints"
-          :key="hint"
-          color="secondary"
-          class="ma-2 ml-4"
-        >
-          {{ hint }}
-        </v-chip>
-      </v-row>
-    </v-card-actions>
+      <v-card-text
+        class="text-h6 font-weight-bold pt-8 mt-8"
+      >{{ data.answer }}</v-card-text>
+
+      <v-card-text
+        class="pt-2 font-italic"
+      >{{ data.explanation }}</v-card-text>
+    </div>
   </v-card>
 </template>
 
--- src/data/english-data.js
+++ src/data/english-data.js
@@ -1,37 +1,43 @@
 const inform = {
-  hints: ["Work English", "A1"],
+  hints: ["Work English", "B1"],
   question: "inform",
   answer: "bilgilendirmek",
+  explanation: "to tell someone about particular facts",
 };
 
 const introduce = {
-  hints: ["Work English", "A2"],
+  hints: ["Work English", "B2"],
   question: "introduce",
   answer: "tanitmak",
+  explanation: "to put something into use, operation, or a place for the first time",
 };
 
 const delay = {
   hints: ["Work English", "A2"],
   question: "delay",
   answer: "gecikmek",
+  explanation: "to not act quickly or immediately",
 };
 
 const treat = {
   hints: ["Hospital", "A2"],
   question: "treat",
   answer: "tedavi",
+  explanation: "to do something to improve the condition of an ill or injured person, or to try to cure a disease",
 };
 
 const seal = {
   hints: ["B1"],
   question: "seal",
   answer: "muhur",
+  explanation: "an official mark on a document, sometimes made with wax, that shows that it is legal or has been officially approved",
 };
 
 const quite = {
   hints: ["Work English", "B1"],
   question: "quite",
   answer: "Oldukca, biraz",
+  explanation: "completely",
 };
 
 export const englishData = [
